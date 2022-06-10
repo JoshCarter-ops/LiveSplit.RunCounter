ef54790c973cc8e586f7520f71c0a4d3e38ab292
cartersoft <aj.carter@hotmail.co.uk>
2022-06-10 16:30:42 +0100

    boilerplate and gitignore updates

---
+++ LiveSplit.RunCounter/UI/Components/RunCounterComponent.cs
@@ -0,0 +1,89 @@
+﻿using LiveSplit.Model;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace LiveSplit.UI.Components
+{
+    public class RunCounterComponent : IComponent
+    {
+        protected InfoTextComponent InternalComponent { get; set; }
+        public RunCounterSettings Settings { get; set; }
+        protected LiveSplitState CurrentState { get; set; }
+        public string ComponentName => "Run Counter";
+
+        public float HorizontalWidth => InternalComponent.HorizontalWidth;
+        public float MinimumWidth => InternalComponent.MinimumWidth;
+        public float VerticalHeight => InternalComponent.VerticalHeight;
+        public float MinimumHeight => InternalComponent.MinimumHeight;
+
+        public float PaddingTop => InternalComponent.PaddingTop;
+        public float PaddingLeft => InternalComponent.PaddingLeft;
+        public float PaddingBottom => InternalComponent.PaddingBottom;
+        public float PaddingRight => InternalComponent.PaddingRight;
+
+        public IDictionary<string, Action> ContextMenuControls => null;
+
+        public RunCounterComponent(LiveSplitState state)
+        {
+
+        }
+
+        public void DrawHorizontal(Graphics g, LiveSplitState state, float height, Region clipRegion)
+        {
+            InternalComponent.NameLabel.HasShadow
+                = InternalComponent.ValueLabel.HasShadow
+                = state.LayoutSettings.DropShadows;
+
+            InternalComponent.NameLabel.ForeColor = state.LayoutSettings.TextColor;
+            InternalComponent.ValueLabel.ForeColor = state.LayoutSettings.TextColor;
+
+            InternalComponent.DrawHorizontal(g, state, height, clipRegion);
+        }
+
+        public void DrawVertical(Graphics g, LiveSplitState state, float width, Region clipRegion)
+        {
+            InternalComponent.DisplayTwoRows = Settings.Display2Rows;
+
+            InternalComponent.NameLabel.HasShadow
+                = InternalComponent.ValueLabel.HasShadow
+                = state.LayoutSettings.DropShadows;
+
+            InternalComponent.NameLabel.ForeColor = state.LayoutSettings.TextColor;
+            InternalComponent.ValueLabel.ForeColor = state.LayoutSettings.TextColor;
+
+            InternalComponent.DrawVertical(g, state, width, clipRegion);
+        }
+
+        public Control GetSettingsControl(LayoutMode mode)
+        {
+            Settings.Mode = mode;
+            return Settings;
+        }
+
+        public System.Xml.XmlNode GetSettings(System.Xml.XmlDocument document)
+        {
+            return Settings.GetSettings(document);
+        }
+
+        public void SetSettings(System.Xml.XmlNode settings)
+        {
+            Settings.SetSettings(settings);
+        }
+
+        public void Update(IInvalidator invalidator, LiveSplitState state, float width, float height, LayoutMode mode)
+        {
+
+        }
+
+        public void Dispose()
+        {
+
+        }
+
+        public int GetSettingsHashCode() => Settings.GetSettingsHashCode();
+    }
+}
